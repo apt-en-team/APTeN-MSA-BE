e4d10836b9cf8c53969899a7f4157a02c1f4fb70
soyoung <ssssy93@icloud.com>
2026-04-22 23:33:16 +0900

    refactor(parking-vehicle): 불필요 캐시 엔티티 및 Kafka 소비 로직 제거

--- parking-vehicle-service/src/main/java/com/apten/parkingvehicle/application/service/ParkingVehicleReferenceCacheService.java
+++ parking-vehicle-service/src/main/java/com/apten/parkingvehicle/application/service/ParkingVehicleReferenceCacheService.java
@@ -1,16 +1,10 @@
 package com.apten.parkingvehicle.application.service;
 
-import com.apten.common.kafka.payload.ApartmentComplexEventPayload;
 import com.apten.common.kafka.payload.HouseholdEventPayload;
-import com.apten.common.kafka.payload.HouseholdMemberEventPayload;
 import com.apten.common.kafka.payload.UserEventPayload;
-import com.apten.parkingvehicle.domain.entity.ApartmentComplexCache;
 import com.apten.parkingvehicle.domain.entity.HouseholdCache;
-import com.apten.parkingvehicle.domain.entity.HouseholdMemberCache;
 import com.apten.parkingvehicle.domain.entity.UserCache;
-import com.apten.parkingvehicle.domain.repository.ApartmentComplexCacheRepository;
 import com.apten.parkingvehicle.domain.repository.HouseholdCacheRepository;
-import com.apten.parkingvehicle.domain.repository.HouseholdMemberCacheRepository;
 import com.apten.parkingvehicle.domain.repository.UserCacheRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -25,14 +19,8 @@ public class ParkingVehicleReferenceCacheService {
     // user cache 저장소
     private final UserCacheRepository userCacheRepository;
 
-    // apartment complex cache 저장소
-    private final ApartmentComplexCacheRepository apartmentComplexCacheRepository;
-
     // household cache 저장소
     private final HouseholdCacheRepository householdCacheRepository;
-
-    // household member cache 저장소
-    private final HouseholdMemberCacheRepository householdMemberCacheRepository;
 
     // 동일 PK가 있으면 갱신하고 없으면 생성한다
     public void upsertUserCache(UserEventPayload payload) {
@@ -43,26 +31,10 @@ public class ParkingVehicleReferenceCacheService {
     }
 
     // 동일 PK가 있으면 갱신하고 없으면 생성한다
-    public void upsertApartmentComplexCache(ApartmentComplexEventPayload payload) {
-        ApartmentComplexCache apartmentComplexCache = apartmentComplexCacheRepository.findById(payload.getApartmentComplexId())
-                .orElseGet(() -> ApartmentComplexCache.builder().apartmentComplexId(payload.getApartmentComplexId()).build());
-        apartmentComplexCache.apply(payload);
-        apartmentComplexCacheRepository.save(apartmentComplexCache);
-    }
-
-    // 동일 PK가 있으면 갱신하고 없으면 생성한다
     public void upsertHouseholdCache(HouseholdEventPayload payload) {
         HouseholdCache householdCache = householdCacheRepository.findById(payload.getHouseholdId())
                 .orElseGet(() -> HouseholdCache.builder().id(payload.getHouseholdId()).build());
         householdCache.apply(payload);
         householdCacheRepository.save(householdCache);
-    }
-
-    // 제거 이벤트도 물리 삭제 대신 상태값을 포함한 upsert로 반영한다
-    public void upsertHouseholdMemberCache(HouseholdMemberEventPayload payload) {
-        HouseholdMemberCache householdMemberCache = householdMemberCacheRepository.findById(payload.getHouseholdMemberId())
-                .orElseGet(() -> HouseholdMemberCache.builder().householdMemberId(payload.getHouseholdMemberId()).build());
-        householdMemberCache.apply(payload);
-        householdMemberCacheRepository.save(householdMemberCache);
     }
 }
--- parking-vehicle-service/src/main/java/com/apten/parkingvehicle/infrastructure/kafka/ParkingVehicleKafkaHandler.java
+++ parking-vehicle-service/src/main/java/com/apten/parkingvehicle/infrastructure/kafka/ParkingVehicleKafkaHandler.java
@@ -2,9 +2,7 @@ package com.apten.parkingvehicle.infrastructure.kafka;
 
 import com.apten.common.kafka.EventEnvelope;
 import com.apten.common.kafka.KafkaTopics;
-import com.apten.common.kafka.payload.ApartmentComplexEventPayload;
 import com.apten.common.kafka.payload.HouseholdEventPayload;
-import com.apten.common.kafka.payload.HouseholdMemberEventPayload;
 import com.apten.common.kafka.payload.UserEventPayload;
 import com.apten.parkingvehicle.application.service.ParkingVehicleReferenceCacheService;
 import lombok.RequiredArgsConstructor;
@@ -28,32 +26,10 @@ public class ParkingVehicleKafkaHandler {
         parkingVehicleReferenceCacheService.upsertUserCache(eventEnvelope.getPayload());
     }
 
-    // apartment complex 이벤트를 받아 단지 cache를 갱신한다
-    @KafkaListener(topics = KafkaTopics.APARTMENT_COMPLEX, groupId = "parking-vehicle-service-apartment-complex-cache")
-    public void consumeApartmentComplexEvent(EventEnvelope<ApartmentComplexEventPayload> eventEnvelope) {
-        log.info(
-                "Consumed apartment complex event. eventType={}, eventId={}",
-                eventEnvelope.getEventType(),
-                eventEnvelope.getEventId()
-        );
-        parkingVehicleReferenceCacheService.upsertApartmentComplexCache(eventEnvelope.getPayload());
-    }
-
     // household 이벤트를 받아 세대 cache를 갱신한다
     @KafkaListener(topics = KafkaTopics.HOUSEHOLD, groupId = "parking-vehicle-service-household-cache")
     public void consumeHouseholdEvent(EventEnvelope<HouseholdEventPayload> eventEnvelope) {
         log.info("Consumed household event. eventType={}, eventId={}", eventEnvelope.getEventType(), eventEnvelope.getEventId());
         parkingVehicleReferenceCacheService.upsertHouseholdCache(eventEnvelope.getPayload());
-    }
-
-    // household member 이벤트를 받아 세대 구성원 cache를 갱신한다
-    @KafkaListener(topics = KafkaTopics.HOUSEHOLD_MEMBER, groupId = "parking-vehicle-service-household-member-cache")
-    public void consumeHouseholdMemberEvent(EventEnvelope<HouseholdMemberEventPayload> eventEnvelope) {
-        log.info(
-                "Consumed household member event. eventType={}, eventId={}",
-                eventEnvelope.getEventType(),
-                eventEnvelope.getEventId()
-        );
-        parkingVehicleReferenceCacheService.upsertHouseholdMemberCache(eventEnvelope.getPayload());
     }
 }
