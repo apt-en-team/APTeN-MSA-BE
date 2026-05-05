d2d0ca3b77b3ee70de1260f32340f2f5d1d1ee1e
gani825 <kgee0503@gmail.com>
2026-05-05 17:06:23 +0900

    fix(auth): AuthLoginPostRes name 필드 오타 수정

--- auth-service/src/main/java/com/apten/auth/application/model/response/AuthLoginPostRes.java
+++ auth-service/src/main/java/com/apten/auth/application/model/response/AuthLoginPostRes.java
@@ -16,7 +16,7 @@ public class AuthLoginPostRes {
     // 사용자 UID
     private final String userUid;
     // 이름
-    private final String namegit;
+    private final String name;
     // 권한
     private final String role;
     // 상태
