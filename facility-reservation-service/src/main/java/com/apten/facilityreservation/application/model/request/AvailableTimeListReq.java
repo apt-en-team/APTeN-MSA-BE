a3afa80e4dc977f2b8fb39ffd1f18e572b65ce6d
soyoung <ssssy93@icloud.com>
2026-05-17 22:50:38 +0900

    fix(facility-reservation): 예약 가능 시간 조회 날짜 바인딩 오류 수정

--- facility-reservation-service/src/main/java/com/apten/facilityreservation/application/model/request/AvailableTimeListReq.java
+++ facility-reservation-service/src/main/java/com/apten/facilityreservation/application/model/request/AvailableTimeListReq.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 // 예약 가능 시간 조회 요청 DTO이다.
 @Getter
@@ -16,6 +17,7 @@ public class AvailableTimeListReq {
     // 시설 ID이다.
     private Long facilityId;
 
-    // 예약일이다.
+    // 예약일이다. yyyy-MM-dd 형식으로 전달해야 한다.
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate reservationDate;
 }
