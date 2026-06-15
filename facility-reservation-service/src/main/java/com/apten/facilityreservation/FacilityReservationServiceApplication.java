b498c1c57dcf32202679edd249ef44690ce04f12
soyoung <ssssy93@icloud.com>
2026-06-15 23:27:53 +0900

    chore(facility-reservation): 미사용 @EnableFeignClients 제거

--- facility-reservation-service/src/main/java/com/apten/facilityreservation/FacilityReservationServiceApplication.java
+++ facility-reservation-service/src/main/java/com/apten/facilityreservation/FacilityReservationServiceApplication.java
@@ -3,10 +3,8 @@ package com.apten.facilityreservation;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableFeignClients
 @EnableJpaAuditing
 @SpringBootApplication
 public class FacilityReservationServiceApplication {
